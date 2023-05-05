d8aaab470b479cf7d63e803d161be7796dd45b78
kohlvbusiness98@gmail.com <kohlvbusiness98@gmail.com>
2023-05-05 15:27:05 +0100

    added media queries

--- src/Components/Calculate/Calculate.js
+++ src/Components/Calculate/Calculate.js
@@ -11,23 +11,26 @@ const Calculate = () => {
     const [year, setYear] = useState('')
     const [event, setEvent] = useState('')
     const [fighter, setFighter] = useState('')
+    const [news, updateNews] = useState([])
     
     const options = {
         method: 'GET',
         url: `https://fightingtomatoes.com/API/627eb84ecbcbf67c8c4cf22249f032a6d60a59fb/${year}/${event}/${fighter}`,
     }
-    axios.request(options).then(function (response) {
-        console.log(response.data)
+    axios.request(options).then(function(response) {
+        updateNews(response.data)
+        console.log(news)
+        //const parsedData = JSON.parse(news)
     }).catch((function (error) {
         console.error(error)
-    }))
-
-
+    }), [])
+    
+    
     return (
         <>
             <Navbar />
-            <h1 className="text-center" style={{ marginTop: '15px' }}>Search for fight news</h1>
-            <div className="calculator-wrapper" />
+            <div className="calculator-wrapper">
+            <h1 className="text-center" >Search for fight news</h1>
             <div className="form-group col-md-6 col-sm-12">
                 <label htmlFor="Year">Enter year: </label>
                 <input type="text" className="form-control" placeholder="2022" name="year" value={year} required onChange={(e) => setYear(e.target.value)} />
@@ -38,11 +41,13 @@ const Calculate = () => {
             </div>
             <div className="form-group col-md-6  col-sm-12">
                 <label htmlFor="Fighter">Enter fighter:  </label>
-                <input type="text" placeholder="UFC 287" className="form-control" name="fighter" value={fighter} required onChange={(e) => setFighter(e.target.value)} />
+                <input type="text" placeholder="Khabib" className="form-control" name="fighter" value={fighter} required onChange={(e) => setFighter(e.target.value)} />
             </div>
             <div className="button-container">
-                <button type="submit" onSubmit={options}>Click</button>
+                <button type="submit" onClick={() => options}>Search</button>
             </div>
+            </div>
+            <div className='block'></div>
             <Footer />
         </>
     )
