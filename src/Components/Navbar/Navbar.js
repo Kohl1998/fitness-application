bbae58aecb64f174d24d498d252f01826195e73d
kohlvbusiness98@gmail.com <kohlvbusiness98@gmail.com>
2023-05-05 15:35:17 +0100

    added media queries

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -5,12 +5,12 @@ import "./Navbar.css"
 const Navbar = () => {
     return (
         <nav className="nav">
-            <a href="#" className="site-title">Fight Zone</a>
-            <ul>
-                <Link to='/home'>Home</Link>
+            <a href="#" className="site-title">Fight Cage</a>
+            <ul className="links">
                 <Link to='/services'>Services</Link>
                 <Link to='/testimonials'>Testimonials</Link>
                 <Link to='/contact'>Contact</Link>
+                <Link to='/calculate'>Fight News</Link>
             </ul>
         </nav>
     )
