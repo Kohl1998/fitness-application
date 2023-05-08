23ea9bbe2f5beafe3a558e58a25f53b48cc45de4
kohlvbusiness98@gmail.com <kohlvbusiness98@gmail.com>
2023-05-08 16:59:10 +0100

    added spacing and padding for tablet users

--- src/Components/Testimonials/Testimonials.js
+++ src/Components/Testimonials/Testimonials.js
@@ -20,22 +20,22 @@ const Testimonials = () => {
                 interval={6100}
             >
                 <div>
-                    <img src="/images/shirley.png" alt="Roger"/>
+                    <img src="/images/shirley.png" alt="Carlos"/>
                     <div className="myCarousel">
-                        <h3>Carlos Pontos</h3>
-                        <h4>Jiu Jitsu/Muay Thai</h4>
-                        <p>
+                        <h3 className="testimonial-name">Carlos Pontos</h3>
+                        <h4 className="testimonial-class">Jiu Jitsu/Muay Thai</h4>
+                        <p className="testimonial-description">
                             I joined Fightzone learning Jiu Jitsu from Theo and Magna which has been an amazing experience, now I do Muay Thai.
                         </p>
                     </div>
                 </div>
 
                 <div>
-                    <img src="/images/daniel.png" alt="Josh"/>
+                    <img src="/images/daniel.png" alt="JKohl"/>
                     <div className="myCarousel">
-                        <h3>Kohl Vernon</h3>
-                        <h4>Jiu Jitsu Gi and NoGi</h4>
-                        <p>
+                        <h3 className="testimonial-name">Kohl Vernon</h3>
+                        <h4 className="testimonial-class">Jiu Jitsu Gi and NoGi</h4>
+                        <p className="testimonial-description">
                             Joining fightzone has been one of the best decisions I have made; learning Jiu Jitsu from Theo and Magna which has boosted my confidence.
                         </p>
                     </div>
@@ -44,9 +44,9 @@ const Testimonials = () => {
                 <div>
                     <img src="/images/theo.png" alt="John"/>
                     <div className="myCarousel">
-                        <h3>John Wick</h3>
-                        <h4>Muay Thai</h4>
-                        <p>
+                        <h3 className="testimonial-name">John Wick</h3>
+                        <h4 className="testimonial-class">Muay Thai</h4>
+                        <p className="testimonial-description">
                             I joined Fight Zone for the Muay Thai, where I met the amazing coach Jose who helped me build my foundation. 
                         </p>
                     </div>
